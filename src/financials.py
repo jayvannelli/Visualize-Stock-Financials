cc19415b31d1f75e03b6a36380814ae7b7220dbe
jayvannelli <jaymvannelli@gmail.com>
2022-09-15 01:11:10 -0400

    update

--- src/financials.py
+++ src/financials.py
@@ -2,10 +2,10 @@ import requests
 import pandas as pd
 import streamlit as st
 
-FMP_BASE_URL = "https://financialmodelingprep.com/api/v3/"
+FMP_BASE_URL = "https://financialmodelingprep.com/api/"
 
 def get_quarterly_income_statements(symbol, last, return_df):
-    req_url = FMP_BASE_URL + f"income-statement/{symbol.upper()}?period=quarter&limit={last}&apikey={st.secrets['FMP_TOKEN']}"
+    req_url = FMP_BASE_URL + f"/v3/income-statement/{symbol.upper()}?period=quarter&limit={last}&apikey={st.secrets['FMP_TOKEN']}"
     req = requests.get(req_url).json()
 
     if return_df == True:
@@ -15,7 +15,7 @@ def get_quarterly_income_statements(symbol, last, return_df):
     return df
 
 def get_annual_income_statements(symbol, last, return_df):
-    req_url = FMP_BASE_URL + f"income-statement/{symbol.upper()}?limit={last}&apikey={st.secrets['FMP_TOKEN']}"
+    req_url = FMP_BASE_URL + f"/v3/income-statement/{symbol.upper()}?limit={last}&apikey={st.secrets['FMP_TOKEN']}"
     req = requests.get(req_url).json()
 
     if return_df == True:
@@ -25,7 +25,7 @@ def get_annual_income_statements(symbol, last, return_df):
     return df
 
 def get_quarterly_balance_sheets(symbol, last, return_df):
-    req_url = FMP_BASE_URL + f"balance-sheet-statement/{symbol.upper()}?period=quarter&limit={last}&apikey={st.secrets['FMP_TOKEN']}"
+    req_url = FMP_BASE_URL + f"/v3/balance-sheet-statement/{symbol.upper()}?period=quarter&limit={last}&apikey={st.secrets['FMP_TOKEN']}"
     req = requests.get(req_url).json()
 
     if return_df == True:
@@ -35,7 +35,7 @@ def get_quarterly_balance_sheets(symbol, last, return_df):
     return df
 
 def get_annual_balance_sheets(symbol, last, return_df):
-    req_url = FMP_BASE_URL + f"balance-sheet-statement/{symbol.upper()}?limit={last}&apikey={st.secrets['FMP_TOKEN']}"
+    req_url = FMP_BASE_URL + f"/v3/balance-sheet-statement/{symbol.upper()}?limit={last}&apikey={st.secrets['FMP_TOKEN']}"
     req = requests.get(req_url).json()
 
     if return_df == True:
@@ -45,7 +45,7 @@ def get_annual_balance_sheets(symbol, last, return_df):
     return df
 
 def get_quarterly_cash_flows(symbol, last, return_df):
-    req_url = FMP_BASE_URL + f"cash-flow-statement/{symbol.upper()}?period=quarter&limit={last}&apikey={st.secrets['FMP_TOKEN']}"
+    req_url = FMP_BASE_URL + f"/v3/cash-flow-statement/{symbol.upper()}?period=quarter&limit={last}&apikey={st.secrets['FMP_TOKEN']}"
     req = requests.get(req_url).json()
 
     if return_df == True:
@@ -55,7 +55,7 @@ def get_quarterly_cash_flows(symbol, last, return_df):
     return df
 
 def get_annual_cash_flows(symbol, last, return_df):
-    req_url = FMP_BASE_URL + f"cash-flow-statement/{symbol.upper()}?limit={last}&apikey={st.secrets['FMP_TOKEN']}"
+    req_url = FMP_BASE_URL + f"/v3/cash-flow-statement/{symbol.upper()}?limit={last}&apikey={st.secrets['FMP_TOKEN']}"
     req = requests.get(req_url).json()
 
     if return_df == True:
